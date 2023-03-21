81d4890b25a39240ac2dc0981ce9dbed70baa7fd
Georgii Bondarenko <bgv900@mail.ru>
2023-03-21 20:04:47 +0300

    ДЗ по семинар № 4 готово

---
+++ Task_25/Program.cs
@@ -0,0 +1,17 @@
+﻿#pragma warning disable
+Console.Clear();
+Console.WriteLine("Напишите цикл, который принимает на вход два числа (A и B) и возводит число A в натуральную степень B.");
+
+Console.WriteLine ("Введите число A");
+int A;
+int.TryParse(Console.ReadLine ()!, out A);
+Console.WriteLine("Введите число B");
+int B;
+int.TryParse(Console.ReadLine ()!, out B);
+int N = 1;
+for (int i = 0; i < B; i++)
+{
+    N = N * A;
+}
+
+Console.WriteLine($"{A} в степени {B} равно -> {N}");
---
+++ Task_27/Program.cs
@@ -0,0 +1,21 @@
+﻿#pragma warning disable
+Console.Clear();
+Console.WriteLine("Напишите программу, которая принимает на вход число и выдаёт сумму цифр в числе.");
+
+Console.WriteLine ("Введите число A");
+int A;
+int.TryParse(Console.ReadLine ()!, out A);
+void sum ()
+{
+int sum = 0;
+int b = A;
+while ( A > 0)
+    {
+    int ost = A % 10;
+    A = A / 10;
+    sum = sum + ost;
+    }
+Console.WriteLine($"{b} -> {sum}" );
+}
+
+sum ();
